74f2654a1e09967efb8d431d84571b37fb25cf11
박선용 <74956749+jerry-world@users.noreply.github.com>
2025-10-20 23:00:12 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 7.81 ms, Memory: 85.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] solution(String s) {
+        String val = s;
+
+        int changeCnt = 0;
+        int totalZeroCnt = 0;
+        while (true) {
+            int zeroCnt = 0;
+
+            StringBuilder sb = new StringBuilder();
+            //0개수 세기 및 1 나열하기
+            for (char c : val.toCharArray()) {
+                if (c == '0') {
+                    zeroCnt++;
+                } else {
+                    sb.append(c);
+                }
+            }
+
+            totalZeroCnt += zeroCnt;
+            if (sb.toString().equals("1")) {
+                changeCnt++;
+                break;
+            } else {
+                val = Integer.toString(sb.length(), 2);
+                changeCnt++;
+            }
+        }
+
+        return new int[]{changeCnt, totalZeroCnt};
+    }
+}
