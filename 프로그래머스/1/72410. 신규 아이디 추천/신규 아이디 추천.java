53c3a34531a182a666a256299b76d47f8ba1aeaf
박선용 <74956749+jerry-world@users.noreply.github.com>
2025-10-20 23:27:27 +0900

    [level 1] Title: 신규 아이디 추천, Time: 17.94 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,46 @@
+class Solution {
+    public String solution(String new_id) {
+        //level1
+        new_id = new_id.toLowerCase();
+
+        //level2
+        new_id = new_id.replaceAll("[[^a-z0-9\\-_.]]", "");
+
+        //level3
+        while (true) {
+            if (new_id.contains("..")) {
+                new_id = new_id.replaceAll("\\.\\.", ".");
+            } else {
+                break;
+            }
+        }
+
+        //level4
+        new_id = new_id.charAt(new_id.length() - 1) == '.' ? new_id.substring(0, new_id.length() - 1) : new_id;
+        if (!new_id.isEmpty()) {
+            new_id = new_id.charAt(0) == '.' ? new_id.substring(1) : new_id;
+        }
+
+        //level5
+        new_id = new_id.isEmpty() ? "a" : new_id;
+
+        //level6
+        if (new_id.length() >= 16) {
+            new_id = new_id.substring(0, 15);
+        }
+
+        //level7
+        char c = new_id.charAt(new_id.length() - 1);
+        while (true) {
+            if (new_id.length() <= 2) {
+                new_id += c;
+            }else{
+                break;
+
+            }
+        }
+        new_id = new_id.charAt(new_id.length() - 1) == '.' ? new_id.substring(0, new_id.length() - 1) : new_id;
+
+        return new_id;
+    }
+}
