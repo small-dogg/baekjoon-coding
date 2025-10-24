6fed70fe34ca483fa4cf02b258241e19a8a6afb9
박선용 <74956749+jerry-world@users.noreply.github.com>
2025-10-25 01:45:28 +0900

    [level 0] Title: 배열 뒤집기, Time: 3.68 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        List<Integer> list = Arrays.stream(num_list).boxed().collect(Collectors.toList());
+        Collections.reverse(list);
+        
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
