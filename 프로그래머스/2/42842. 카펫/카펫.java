01a7f3c22895cd76de95ad9ab60119c6a60d5cb3
박선용 <74956749+jerry-world@users.noreply.github.com>
2025-10-24 18:14:15 +0900

    [level 2] Title: 카펫, Time: 13.00 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int surface = brown + yellow;
+
+        List<Square> devisor = getDevisor(surface);
+        for (Square square : devisor) {
+            if (square.x * 2 + (square.y - 2) * 2 == brown) {
+                return new int[]{square.y, square.x};
+            }
+        }
+        return new int[]{};
+    }
+
+    private List<Square> getDevisor(int surface) {
+        List<Square> squares = new ArrayList<>();
+        for (int i = 3; i < surface - 2; i++) {
+            if (surface % i == 0) {
+                squares.add(new Square(i, surface / i));
+            }
+        }
+
+        return squares;
+    }
+
+    public class Square {
+        int x;
+        int y;
+
+        public Square(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
