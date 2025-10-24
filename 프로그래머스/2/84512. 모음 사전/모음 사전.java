f32480408f477e47bc0ab562350a61f5cc32af75
박선용 <74956749+jerry-world@users.noreply.github.com>
2025-10-24 10:30:17 +0900

    [level 2] Title: 모음 사전, Time: 35.95 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    private static final char[] AEIOU = new char[]{'A', 'E', 'I', 'O', 'U'};
+
+    public int solution(String word) {
+        List<String> words = new ArrayList<>();
+        generate("", words);
+
+        return words.indexOf(word);
+    }
+
+    private void generate(String word, List<String> list) {
+        // 종료조건
+        if (word.length() > 5) return;
+
+        list.add(word);
+        for (char c : AEIOU) {
+            generate(word + c, list);
+        }
+    }
+}
