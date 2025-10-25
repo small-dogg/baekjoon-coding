5b22a3a3c0b5de5ce4459df534b2bde9ec1eb26b
박선용 <74956749+jerry-world@users.noreply.github.com>
2025-10-26 01:18:32 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 1.07 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -0,0 +1,60 @@
+import java.util.*;
+// 구현 점수 : C
+class Solution {
+    
+    //총 6개의 연산자 순서를 배열로 정의
+    private static final String[][] precedences = {
+            "+-*".split(""),
+            "+*-".split(""),
+            "-+*".split(""),
+            "-*+".split(""),
+            "*+-".split(""),
+            "*-+".split("")
+    };
+
+    public long solution(String expression) {
+        StringTokenizer tokenizer = new StringTokenizer(expression, "+-*", true);
+        List<String> tokens = new ArrayList<>();
+        while (tokenizer.hasMoreTokens()) {
+            tokens.add(tokenizer.nextToken());
+        }
+
+        long max = 0;
+        for (String[] precedence : precedences) {
+            long value = Math.abs(calculate(new ArrayList<>(tokens), precedence));
+            if (value > max) max = value;
+
+        }
+        return max;
+    }
+
+    //두 피연산자와 연산자를 받아 연산 결과를 반환
+    private long calculate(long lhs, long rhs, String op) {
+        return switch (op) {
+            case "+" -> lhs + rhs;
+            case "-" -> lhs - rhs;
+            case "*" -> lhs * rhs;
+            default -> 0;
+        };
+    }
+
+    private long calculate(List<String> tokens, String[] precedence) {
+        for (String op : precedence) {
+            for (int i = 0; i < tokens.size(); i++) {
+                if(tokens.get(i).equals(op)) {
+                    long lhs = Long.parseLong(tokens.get(i - 1));
+                    long rhs = Long.parseLong(tokens.get(i + 1));
+                    long result = calculate(lhs, rhs, op);
+
+                    tokens.remove(i - 1);
+                    tokens.remove(i - 1);
+                    tokens.remove(i - 1);
+                    tokens.add(i - 1, String.valueOf(result));
+                    i -= 2;
+                }
+            }
+        }
+
+        return Long.parseLong(tokens.get(0));
+    }
+}
