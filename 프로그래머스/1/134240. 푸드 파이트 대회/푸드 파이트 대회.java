dc742a6a9f2e483db0cf9ec55e78e5748ee889ee
jerry-world <sy920112@naver.com>
2025-10-31 15:24:25 +0900

    chore: repeat

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -19,4 +19,14 @@ class Solution {
         }
         return sb.toString();
     }
+
+//    public String solution(int[] food) {
+//        StringBuilder builder = new StringBuilder();
+//        for (int i=1; i<food.length; i++) {
+//            int result = food[i] / 2;
+//            builder.append(String.valueOf(i).repeat(result));
+//        }
+//        String answer = builder + "0";
+//        return answer + builder.reverse();
+//    }
 }
