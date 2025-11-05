7a7712f3c5d9998f99086f8d15e0e007d9f57b27
박선용 <74956749+small-dogg@users.noreply.github.com>
2025-11-05 16:17:05 +0900

    [level 3] Title: 정수 삼각형, Time: 25.22 ms, Memory: 63.8 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,23 +1,27 @@
 import java.util.*;
 
 class Solution {
-    private final static int[][] mem = new int[501][501];
-    
     public int solution(int[][] triangle) {
-        for(int[] arr : mem){
-            Arrays.fill(arr, -1);
+
+        //삼각형의 높이는 최대 500
+        int[][] memo = new int[501][501];
+        for (int[] ints : memo) {
+            Arrays.fill(ints, -1);
         }
-        
-        return max(0, 0, triangle);
+
+        return getMaxSum(0, 0, triangle, memo);
     }
-    
-    private int max(int x, int y, int[][] triangle) {
-        if(y == triangle.length) return 0;
-        if(mem[x][y] != -1) return mem[x][y];
-        
-        return mem[x][y] = triangle[y][x] + Math.max(
-            max(x, y+1, triangle),
-            max(x+1, y+1, triangle)
-        );
+
+    private int getMaxSum(int y, int x, int[][] triangle, int[][] memo) {
+        if (y == triangle.length - 1) {
+            return triangle[y][x];
+        }
+
+        if (memo[y][x] != -1) return memo[y][x];
+
+        int down = getMaxSum(y + 1, x, triangle, memo);
+        int downRight = getMaxSum(y + 1, x + 1, triangle, memo);
+
+        return memo[y][x] = triangle[y][x] + Math.max(down, downRight);
     }
 }
