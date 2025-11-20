49fdcb1cee15b3fd6245d6fd432314ab28baa9cb
박선용 <74956749+small-dogg@users.noreply.github.com>
2025-11-20 20:55:00 +0900

    [level 1] Title: 덧칠하기, Time: 1.43 ms, Memory: 92.5 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,14 +1,6 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int count = 0;
-        int next = 0;
-        for (int i : section) {
-            if (i > next) {
-                next = i + m - 1;
-                count++;
-            }
-        }
-
-        return count;
+        int answer = 0;
+        return answer;
     }
 }
