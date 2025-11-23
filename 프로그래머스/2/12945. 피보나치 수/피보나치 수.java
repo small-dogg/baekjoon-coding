cdc2bb767a97a5dbfbf54c0357d78fab32638bc6
박선용 <74956749+small-dogg@users.noreply.github.com>
2025-11-23 14:53:16 +0900

    [level 2] Title: 피보나치 수, Time: 6.25 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,21 +1,15 @@
-import java.util.*;
-
 class Solution {
-    private static final int[] memo = new int[100_001];
+    static int[] memo;
+
     public int solution(int n) {
-        Arrays.fill(memo, -1);
-        
-        for (int i = 0; i <= n; i++){
-            fibo(n);    
-        }
-        
+        memo = new int[n + 1];
         return fibo(n);
     }
-    
+
     private int fibo(int n) {
-        if(memo[n] != -1) return memo[n];
-        if(n == 0 || n == 1) return n;
-        
-        return memo[n] = (fibo(n - 1) + fibo(n - 2)) % 1234567;
+        if (n < 2) return n;
+        if (memo[n] != 0) return memo[n];
+        memo[n] = (fibo(n - 2) + fibo(n - 1))%1234567;
+        return memo[n];
     }
 }
